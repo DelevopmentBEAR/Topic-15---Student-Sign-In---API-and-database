5eff69c7c8bfa64f8c8d79ada1da9f88818c1d5c
Berit Haugen <movies.tv.and.music@gmail.com>
2026-05-04 17:43:55 -0500

    #

--- server.js
+++ server.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const app = express(); // Creates an express/web app server application
 
+const path = require('path'); // Import the path module for handling file paths
+
+const staticFilesPath = path.join(__dirname, 'client', 'dist'); // Define the path to the public directory for static files
+
+const staticFiles = express.static(staticFilesPath); // Create middleware to serve static files from the defined path
+app.use('/',staticFiles); // Use the static file middleware to serve static files from the specified directory
+// request to home page, serve static file - the vue app index.app from the dist folder
+
 // Import the API routes from routes/api.js
 const apiRoutes = require('./routes/api.js');
 // Use the API routes for any requests to paths that start with /api
